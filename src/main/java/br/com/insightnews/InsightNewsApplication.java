e92e93355c516ce2b9c015e9e8ecc444d7ec5667
Anna <annaclararussoluca@gmail.com>
2025-07-28 22:53:08 -0300

    Criando projeto insight news

---
+++ src/main/java/br/com/insightnews/InsightNewsApplication.java
@@ -0,0 +1,13 @@
+package br.com.insightnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsightNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/insightnews/InsightNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.insightnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
